6015a394c82e5d6a398dd79ea1a892fb68d4b5cf
sumancoding <progress.suman@gmail.com>
2022-05-15 16:34:04 +0300

    home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Header from "../../components/header/Header";
 import NavBar from "../../components/navbar/NavBar";
+import Featured from "../../components/featured/Featured";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+import Footer from "../../components/footer/Footer";
 
 const Home = () => {
   return (
     <div>
       <NavBar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
+        <MailList />
+        <Footer />
+      </div>
     </div>
   );
 };
